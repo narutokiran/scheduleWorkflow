ef65c61cdfa8f30a6d7be48eb667ffaf6d6be0ab
kiran <kiran@kiran-VirtualBox.(none)>
2014-11-17 16:59:51 -0500

    Basic workflow xml scheme

--- src/scheduleWorkflow/submitWorkFlowInitialPhase.java
+++ src/scheduleWorkflow/submitWorkFlowInitialPhase.java
@@ -8,6 +8,15 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+import java.io.*;
+import java.util.*;
+
 public class submitWorkFlowInitialPhase {
 
 	public static void main(String args[])throws Exception
@@ -25,32 +34,43 @@ public class submitWorkFlowInitialPhase {
 			 
 				System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 			 
-				NodeList nList = doc.getElementsByTagName("staff");
+				NodeList workflowList = doc.getElementsByTagName("workflow");
 			 
-				System.out.println("----------------------------");
+				for (int workflow = 0; workflow < workflowList.getLength(); workflow++) {
 			 
-				for (int temp = 0; temp < nList.getLength(); temp++) {
+					Node workflowDetail = workflowList.item(workflow);
+					if (workflowDetail.getNodeType() == Node.ELEMENT_NODE) {
 			 
-					Node nNode = nList.item(temp);
-			 
-					System.out.println("\nCurrent Element :" + nNode.getNodeName());
-			 
-					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-			 
-						Element eElement = (Element) nNode;
-			 
-						System.out.println("Staff id : " + eElement.getAttribute("id"));
-						System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-						System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-						System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-						System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
-			 
+						Element workflowElement = (Element) workflowDetail;
+						String workflowName=new String(workflowElement.getElementsByTagName("workflowName").item(0).getTextContent());
+						String jobsJarPath=new String(workflowElement.getElementsByTagName("jobsJar").item(0).getTextContent());
+						NodeList jobs=workflowElement.getElementsByTagName("job");
+						System.out.println(workflowName+" has submitted to YARN cluster");
+						for(int job=0;job<jobs.getLength();job++)
+						{
+							Node jobDetail= jobs.item(job);
+							if(jobDetail.getNodeType() == Node.ELEMENT_NODE)
+							{
+								Element jobElement= (Element) jobDetail;
+								String jobName=new String(jobElement.getElementsByTagName("jobName").item(0).getTextContent());
+								String mapperClass=new String(jobElement.getElementsByTagName("mapperClass").item(0).getTextContent());
+								String reducerClass=new String(jobElement.getElementsByTagName("reducerClass").item(0).getTextContent());
+								String combinerClass=new String(jobElement.getElementsByTagName("combinerClass").item(0).getTextContent());
+								String outputKeyFormat=new String(jobElement.getElementsByTagName("outputKeyClass").item(0).getTextContent());
+								String outputValueFormat=new String(jobElement.getElementsByTagName("outputValueClass").item(0).getTextContent());
+								String inputDir=new String(jobElement.getElementsByTagName("inputDir").item(0).getTextContent());
+								String outputDir=new String(jobElement.getElementsByTagName("outputDir").item(0).getTextContent());
+								String inputFormatClass=new String(jobElement.getElementsByTagName("inputFormatClass").item(0).getTextContent());
+								String outputFormatClass=new String(jobElement.getElementsByTagName("outputFormatClass").item(0).getTextContent());
+								
+							}
+						}
+						
 					}
 				}
 			    } catch (Exception e) {
 				e.printStackTrace();
 			    }
-
 	}
 }
 
